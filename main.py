579b042636691bef26a263d5dac643dfbb2c4a3a
shivansh <mshivanshmaurya@gmail.com>
2025-04-01 13:53:16 +0530

    commit sdf

--- main.py
+++ main.py
@@ -2,5 +2,14 @@ import random
 import numpy as np
 import tensorflow as tf
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM , Dense, Activation
+from tensorflow.keras.optimizers import RMSprop
 
+filepath = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+text = open(filepath,'rb').read().decode(encoding='utf-8').lower()
+
+text = text[300000:800000]
+
+characters = sorted(set(text))
 
