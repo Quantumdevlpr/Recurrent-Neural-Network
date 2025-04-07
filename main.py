97deb5a9a78318d6348dcd2e3011d9eb3c189ba9
shivansh <mshivanshmaurya@gmail.com>
2025-04-07 14:42:30 +0530

    completing the model

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import random 
+import random
 import numpy as np
 import tensorflow as tf
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM , Dense, Activation
+from tensorflow.keras.layers import LSTM, Dense, Activation
 from tensorflow.keras.optimizers import RMSprop
+
 
 filepath = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
 text = open(filepath,'rb').read().decode(encoding='utf-8').lower()
@@ -26,5 +27,23 @@ for i in range(0 , len(text) - SEQ_LENGTH , STEP_SIZE):
     sentences.append(text[i:i+SEQ_LENGTH])
     next_characters.append(text[i+SEQ_LENGTH])
     
-x = np.zeros((len(sentences), SEQ_LENGTH, len(characters)), dtype=np.bool_)
-y= np.zeros((len(sentences), len(characters)), dtype=np.bool_)
+x = np.zeros((len(sentences), SEQ_LENGTH, len(characters)), dtype=np.bool)
+y= np.zeros((len(sentences), len(characters)), dtype=np.bool)
+
+for i, sentence in enumerate(sentences):
+    for t,character in enumerate(sentence):
+        x[i,t,char_to_index[character]] = 1
+    y[i,char_to_index[next_characters[i]]] = 1
+    
+    
+model = Sequential()
+model.add(LSTM(128, input_shape=(SEQ_LENGTH, len(characters))))
+model.add(Dense(len(characters)))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer=RMSprop(learning_rate=0.01))
+
+
+model.fit(x, y, batch_size=256, epochs=4)
+
+model.save('shakespeare_model')
